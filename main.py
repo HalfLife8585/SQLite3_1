cee3002c55d625e2ed51460a9b92633a2e54a327
HalfLife85 <aleksandrmusic@gmail.com>
2024-09-04 10:15:18 +0300

    my_scnd_comm

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ DB_NAME = "sqlite_db.db"
 #     print(sqlite_conn)
 #     print(sqlite3.version)
 
+courses = [
+    (351, "JavaScript course", 415, 100),
+    (614, "C++ course", 161, 10),
+    (721, "Java full course", 100, 35)
+]
+
 with sqlite3.connect(DB_NAME) as sqlite_conn:
     sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
-    sqlite_conn.execute(sql_request, (251, "Python course", 100, 30))
+    for course in courses:
+        sqlite_conn.execute(sql_request, course)
     sqlite_conn.commit()
 
 

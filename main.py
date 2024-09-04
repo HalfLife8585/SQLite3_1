64c69421d407d9f0a08d3fb7ba09967030f75bb1
HalfLife85 <aleksandrmusic@gmail.com>
2024-09-04 10:10:23 +0300

    my_scnd_comm

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ DB_NAME = "sqlite_db.db"
 #     print(sqlite3.version)
 
 with sqlite3.connect(DB_NAME) as sqlite_conn:
-    sql_request = """CREATE TABLE IF NOT EXISTS courses (
-    id integer PRIMARY KEY,
-    title text NOT NULL,
-    students_qty integer, 
-    reviews_qty integer
-    );"""
+    sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
+    sqlite_conn.execute(sql_request, (251, "Python course", 100, 30))
+    sqlite_conn.commit()
 
-sqlite_conn.execute(sql_request)
+
+
+
+
+# # Create new table
+# with sqlite3.connect(DB_NAME) as sqlite_conn:
+#     sql_request = """CREATE TABLE IF NOT EXISTS courses (
+#     id integer PRIMARY KEY,
+#     title text NOT NULL,
+#     students_qty integer,
+#     reviews_qty integer
+#     );"""
+#
+# sqlite_conn.execute(sql_request)
